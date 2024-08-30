e6f8b010006f3f0eed60337477137d425746dae5
allked@msn.com <allked>
2024-08-30 12:05:01 -0700

    updated doc 08-30

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['initlog_0',['initLog',['../function_library_8cpp.html#a8499fd0b35db9ed427744975703cdeac',1,'functionLibrary.cpp']]],
-  ['inittranscribe_1',['initTranscribe',['../function_library_8cpp.html#ab975e876835d78a77da1e090fda8e04c',1,'initTranscribe(const char *modelName, const char *logFilePath=nullptr):&#160;functionLibrary.cpp'],['../function_library_8h.html#aaffc77dd44d188debdc2c8e318d4ca5d',1,'initTranscribe(const char *modelName, const char *logFilePath):&#160;functionLibrary.cpp']]],
-  ['inittrascribe_2',['initTrascribe',['../function_library_8h.html#a2ef4126df49d04e304f29992faaa25a5',1,'functionLibrary.h']]]
+  ['inittranscribe_1',['initTranscribe',['../function_library_8cpp.html#ab975e876835d78a77da1e090fda8e04c',1,'initTranscribe(const char *modelName, const char *logFilePath=nullptr):&#160;functionLibrary.cpp'],['../function_library_8h.html#aaffc77dd44d188debdc2c8e318d4ca5d',1,'initTranscribe(const char *modelName, const char *logFilePath):&#160;functionLibrary.cpp']]]
 ];
